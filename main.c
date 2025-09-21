bdd25676fb9634aa4984305ef0e28eac5cbdb108
Juliana <cjota.ramos@gmail.com>
2025-09-21 18:57:28 -0300

    merge

--- main.c
+++ main.c
@@ -230,11 +230,8 @@ int main() {
         printf("Sorte: %d. ", sorte5);
     
         if (sorte5 >= 15) {
-<<<<<<< HEAD
+
         printf("Sucesso no ataque! A faca vai diretamente na garganta do dragão, onde há um pequeno espaço que não é revestido pelas escamas. O dragão cai, você o golpeia mais vezes.\n");
-=======
-        printf("Sucesso no ataque! A faca vai diretamente na garganta do dragão, onde há um pequeno espaço que não é revestido pelas escamas. O dragão cai e você o golpeia mais vezes.\n");
->>>>>>> b0586eae229bc69d5219aec52da172128a365f7a
         }  else {
         // Verifica condições anteriores
             if (escolha6 != 1 && escolha9 != 1) {  // Caminho longo E dormiu
@@ -490,7 +487,6 @@ int fazerMagiaNegra() {
     while (escolha != 1 && escolha !=2);
     return escolha;
 }
-<<<<<<< HEAD
 
 int comerPesadelos() {
     int escolha;
@@ -503,5 +499,3 @@ int comerPesadelos() {
     while (escolha != 1 && escolha !=2);
     return escolha;
 }
-=======
->>>>>>> b0586eae229bc69d5219aec52da172128a365f7a
